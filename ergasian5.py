24753df3d75c2146c02c8f1ef364818873324212
StavrosTZI <61393472+StavrosTZI@users.noreply.github.com>
2020-02-26 19:55:50 +0200

    Update ergasian5.py

--- ergasian5.py
+++ ergasian5.py
@@ -1,11 +1,13 @@
 eis=input("Δηλώστε το όνομα ή την διαδρομή του αρχείου: " )
-with open(eis,"r+") as keim:
-  lista=keim.split()#lista: το κειμενο χωρισμενο σε μορφη λιστας
-  for ep in range(len(lista)):#μια επαναληψη για καθε καμματι της λιστας
-    if len(lista[ep])>3:
-      temp1=lista[ep]
-      temp2=temp1[0]
-      sl=len(temp1)
-      temp1=temp1[1:sl]#Χρησιμοποιω slicing για να "κοψω" τον πρωτο χαρακτηρα της λεξης
-      temp1=temp1 + temp2 + "ay"
-      print(temp1)
+fo=open(eis,"r", encoding="utf8",)
+keim=fo.read().rstrip("\n")
+lista=keim.split()#lista: το κειμενο χωρισμενο σε μορφη λιστας
+for ep in range(len(lista)):#μια επαναληψη για καθε καμματι της λιστας
+  if len(lista[ep])>3:
+    temp1=lista[ep]#temp1:λεξη
+    temp2=temp1[0]#temp2:πρωτο γραμμα της λεξης
+    sl=len(temp1)
+    temp1=temp1[1:sl]#Χρησιμοποιω slicing για να "κοψω" τον πρωτο χαρακτηρα της λεξης
+    temp1=temp1 + temp2 + "ay"
+    print(temp1)
+fo.close
