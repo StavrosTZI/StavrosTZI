26e561f8b57f15cfc28245ffda33f3d3efb2dbdc
StavrosTZI <61393472+StavrosTZI@users.noreply.github.com>
2020-02-26 19:54:02 +0200

    Update ergasian2.py

--- ergasian2.py
+++ ergasian2.py
@@ -1,9 +1,10 @@
 eis=input("Δηλώστε το όνομα ή την διαδρομή του αρχείου: " )
-with open(eis,"r+") as keim:
-  spakeim=keim.split()#spasmeno keimeno
-for epan in range(len(spakeim)):#epanalipsi
+fo=open(eis,"r", encoding="utf8",)
+keim=fo.read().rstrip("\n")
+spakeim=keim.split()
+for epan in range(len(spakeim)):#epan:epanalipsi
   leks=spakeim[epan]
-  spaleks=list(leks)#leksi  spasmenh leksh
+  spaleks=list(leks)#leks:leksh  spaleks:spasmenh leksh
   badl=["f","c","k","r"] 
   goodl=["b","d","g","h","j","l","m","n","p","q","s","t","v","w","x","z"]
   bad=0
@@ -11,18 +12,18 @@ for epan in range(len(spakeim)):#epanalipsi
   ss=False
   for epan2 in range(len(spaleks)):
     for epan3 in badl:
-      if spaleks[epan2]==epan3:
+      if spaleks[epan2]==epan3:#Μετρηση κακων γραμματων
         bad=bad + 1
-        ss=True
-    if ss==False:
-      for epan3 in goodl:
-        if spaleks[epan2]==epan3:
-          good=good + 1
+    for epan3 in goodl:
+      if spaleks[epan2]==epan3:#Μετρηση καλων γραμματων
+        good=good + 1
   spakeim[epan]=""
   for epan2 in spaleks:
     spakeim[epan] += epan2
   if bad>good:
-    symp="Καλή"#symperasma
+    symp="Κακή"#symp:symperasma
   else:
-    symp="Κακή"
+    symp="Καλή"
   print (spakeim[epan]+":"+symp)
+  fo.close
+  
